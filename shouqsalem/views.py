de49330e83a2f0ae18d781001bdbbc2987005365
Shouq <shouqsam355@gmail.com>
2025-10-04 23:46:19 +0300

    تعديلات تصميم ونقله الى لابتوب

--- shouqsalem/views.py
+++ shouqsalem/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 def home_view(request):
-    return render(request, "home.html")
+    """
+    الصفحة الرئيسية للمتجر:
+    - تعرض المنتجات الرقمية
+    - تعرض المنتجات الملموسة
+    """
+    digital_products = Product.objects.filter(category__name__icontains="منتجات رقمي")
+    physical_products = Product.objects.filter(category__name__icontains="ملموسة")
+
+    context = {
+        "digital_products": digital_products,
+        "physical_products": physical_products,
+    }
+    return render(request, "home.html", context)
